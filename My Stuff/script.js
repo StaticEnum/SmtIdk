e5e6bd22a3ac07c17be1ec9b0e5a23c808d18dd1
St3ne <95201420+StaticEnum@users.noreply.github.com>
2024-01-07 15:11:25 +0700

    Delete My Stuff directory

--- My Stuff/script.js
+++
@@ -1,5 +0,0 @@
-function changeTitle(titleID, multiplier){
-    document.getElementById(titleID).textContent = Math.ceil(multiplier * Math.random());
-}
-
-setInterval(changeTitle, 1, "title", 1000000000);
